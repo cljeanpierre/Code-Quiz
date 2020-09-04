783d3191b68301a24fa529865172756804aad76a
Carla <carla@layc-dc.org>
2020-09-04 15:59:08 -0400

    added pseudocode for layout of game play

---
+++ script.js
@@ -0,0 +1,10 @@
+// Create a Start Quiz button with a click option
+// Create a timer that starts at 3 minutes and begins counting down when I click the Start Quiz button
+// Make the timer deduct 15 seconds when a question is answered incorrectly
+// Prompt User with first question
+// When the User answers the first question, prompt second question
+// When the User answers the second question, prompt third question
+// When the User answers the third question, prompt fourth question
+// When the User answers the fourth question, prompt fifth question
+// End game when all five questions have been answered or when the timer reaches zero.
+// Allow User to save their initials and score
