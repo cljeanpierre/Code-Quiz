da790fa88129d7fec26c08ab57e3397ae0aad688
Carla <carla@layc-dc.org>
2020-09-06 15:29:25 -0400

    console logged ensure page is connnected to display

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 // Create a timer that starts at 3 minutes and begins counting down when I click the Start Quiz button
 // Make the timer deduct 15 seconds when a question is answered incorrectly
 // Prompt User with first question
-// When the User answers the first question, prompt second question
-// When the User answers the second question, prompt third question
-// When the User answers the third question, prompt fourth question
-// When the User answers the fourth question, prompt fifth question
-// End game when all five questions have been answered or when the timer reaches zero.
+// When the User answers the first question, open a new page and prompt second question
+// When the User answers the second question, open a new page and prompt third question
+// When the User answers the third question, open a new page and prompt fourth question
+// When the User answers the fourth question, open a new page and prompt fifth question
+// End game when all five questions have been answered or when the timer reaches zero
 // Allow User to save their initials and score
+
+console.log ("This page is connected.");
