ebddb7bb1462786794529af2c92e5f79fd269608
Carla <carla@layc-dc.org>
2021-02-01 10:32:33 -0500

    set up global variables

---
+++ quiz.js
@@ -0,0 +1,39 @@
+//set up global variables
+
+let [counter, currentQuestion, correct, incorrect, unanswered] = [20, 0, 0, 0, 0];
+
+//ex. question index, quiz time, ... 
+
+//set up your question array
+
+
+
+//start quiz function
+
+
+
+
+// grab the question function
+
+
+
+
+//decides if the user made a wrong/right click
+
+
+
+
+// function to end quiz
+
+
+
+
+
+//save high score function
+
+
+
+
+
+//set up click events
+
