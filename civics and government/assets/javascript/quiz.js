ef43ec9795f8a1e0b30d460b52909e3b50aeefdc
Carla <carla@layc-dc.org>
2021-02-02 20:45:11 -0500

    added div to show user score and added functionality to show score when user clicks submit

--- civics and government/assets/javascript/quiz.js
+++ civics and government/assets/javascript/quiz.js
@@ -1,20 +1,47 @@
-const correctAnswers = ['C', 'A', 'C', 'C', 'D', 'C', 'C', 'D', 'B', 'B', 'D'];
-
+const correctAnswers = ['C', 'A', 'C', 'C', 'D', 'C', 'C', 'D', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     let score = 0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value, form.q11.value];
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value]
 
     //check answers
     userAnswers.forEach((answer, index) => {
         if(answer === correctAnswers[index]) {
-            score += 25;
+            score += 10;
         }
     });
 
-console.log(score)
+    //show result on page - will include score and deduced time
+    scrollTo(0,0);
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('d-none');
+
+    let output = 0;
+    const time = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score) {
+            clearInterval(timer);
+        } else {
+            output ++;
+        }
+    }, 10);
+
+
+
+    // setTimeout(() => {
+
+    // }, 3000);
+
+    // let i = 0;
+    // const time = setInterval(() => {
+    //     i++;
+    //     if(i===5){
+    //         clearInterval(time);
+    //     }
+    // }, 1000);
 
 });
